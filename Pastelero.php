cd0d3a7d8e0fcf310376d0c0f6cdb275e4e2fa7f
Francisco Rendón <99094137+FranciscoRendon@users.noreply.github.com>
2022-03-05 23:45:24 -0600

    Add files via upload

---
+++ Pastelero.php
@@ -0,0 +1,41 @@
+<?php
+#variable texto
+$palabra= "Velas 0";
+echo "Estas son las velas de tipo 0 año: $palabra";
+echo "<br>";
+$palabra= "Velas 1";
+echo "Estas son las velas de tipo 1 año: $palabra"; 
+echo "<br>";
+$palabra= "Velas 2";
+echo "Estas son las velas de tipo 2 año: $palabra";
+echo "<br>";
+$palabra= "Velas 3";
+echo "Estas son las velas de tipo 4 año: $palabra";
+echo "<br>";
+$palabra= "Velas 5";
+echo "Estas son las velas de tipo 5 año: $palabra";
+echo "<br>";
+$palabra= "Velas 6";
+echo "Estas son las velas de tipo 6 año: $palabra";
+
+echo "<br>";
+$palabra= "Velas 7";
+echo "Estas son las velas de tipo 7 año: $palabra";
+echo "<br>";
+$palabra= "Velas 8";
+echo "Estas son las velas de tipo 8 año: $palabra";
+echo "<br>";
+$palabra= "Velas 9";
+echo "Estas son las velas de tipo 9 año: $palabra";
+echo "<br>";
+$palabra= "Fruta estra";
+echo "Estas son Fruta estra: $palabra";
+echo "<br>";
+$palabra= "Chocolate extra";
+echo "Estas son Chocolate extra: $palabra";
+echo "<br>";
+$palabra= "Dedicatoria decorativa";
+echo "Estas son Dedicatoria decorativa: $palabra";
+?>
+
+
